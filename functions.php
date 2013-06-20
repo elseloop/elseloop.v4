08294dca6393e42b26b92d021cc2b814f0b2de81
Dan Manchester <dan@needmoredesigns.com>
2013-06-20 10:25:32 -0700

    Typos

--- functions.php
+++ functions.php
@@ -11,14 +11,14 @@
  */
 
 
-require_once( trailingslashit( get_theme_directory_uri() ) . 'inc/init.php' );
-require_once( trailingslashit( get_theme_directory_uri() ) . 'inc/scripts.php' );
-require_once( trailingslashit( get_theme_directory_uri() ) . 'inc/menus.php' );
-require_once( trailingslashit( get_theme_directory_uri() ) . 'inc/pagination.php' );
-require_once( trailingslashit( get_theme_directory_uri() ) . 'inc/sidebars.php' );
-require_once( trailingslashit( get_theme_directory_uri() ) . 'inc/excerpts.php' );
-require_once( trailingslashit( get_theme_directory_uri() ) . 'inc/comments.php' );
-require_once( trailingslashit( get_theme_directory_uri() ) . 'inc/classes.php' );
-require_once( trailingslashit( get_theme_directory_uri() ) . 'inc/helpers.php' );
-require_once( trailingslashit( get_theme_directory_uri() ) . 'inc/services.php' );
-require_once( trailingslashit( get_theme_directory_uri() ) . 'inc/shortcodes.php' );
+require_once locate_template( 'inc/init.php' );
+require_once locate_template( 'inc/scripts.php' );
+require_once locate_template( 'inc/menus.php' );
+require_once locate_template( 'inc/pagination.php' );
+require_once locate_template( 'inc/sidebars.php' );
+require_once locate_template( 'inc/excerpts.php' );
+require_once locate_template( 'inc/comments.php' );
+require_once locate_template( 'inc/classes.php' );
+require_once locate_template( 'inc/helpers.php' );
+require_once locate_template( 'inc/services.php' );
+require_once locate_template( 'inc/shortcodes.php' );
--- inc/excerpts.php
+++ inc/excerpts.php
@@ -12,9 +12,8 @@ function needmore_new_excerpt_length($length) {
 add_filter('excerpt_length', 'needmore_new_excerpt_length');
 
 // Stripping [...] from the more link on automatic post excerpts
-  function needmore_excerpt_more($more) {
-    return '&hellip;';
-  }
-  add_filter('excerpt_more', 'needmore_excerpt_more');
+function needmore_excerpt_more($more) {
+  return '&hellip;';
+}
 
-endif;
+add_filter('excerpt_more', 'needmore_excerpt_more');
